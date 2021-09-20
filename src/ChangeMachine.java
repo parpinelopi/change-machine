0d3d0223ca426e39390138fd7f77ef774cc62a2f
parpinelopi <pennyblack.p@gmail.com>
2021-09-20 14:05:54 +0300

    complete function/return statement needs amendment

---
+++ src/ChangeMachine.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class ChangeMachine {
+
+    public static Integer countChange(Integer money, List<Integer> coins, Integer arrLength) {
+
+
+        Integer[] count = new Integer[money + 1];
+        Arrays.fill(count, 0);
+        count[0] = 1;
+
+
+        for (int i = 0; arrLength > i; i++)
+            for (int j = coins.get(i); j < arrLength; j++)
+                count[j] += count[j - coins.get(i)];
+        return count[money];
+    }
+
+    public static void main(String[] args) {
+
+        Integer arr[] = {1, 2, 3};
+        Integer arrLength = arr.length;
+        Integer money = 15;
+        System.out.println(countChange(money, Arrays.asList(arr), arrLength));
+
+
+    }
+
+
+}
+
+
+
+
+
